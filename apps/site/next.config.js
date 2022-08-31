56081046824f80628ce68ed5a0689c4491ac2f78
Siahn Pasion <71996763+siahnpasion@users.noreply.github.com>
2022-08-31 16:10:54 +0800

    Setup tailwind and next-translate. (#5)

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -2,16 +2,17 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
+const nextTranslate = require('next-translate');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true,
   },
 };
 
-module.exports = withNx(nextConfig);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+module.exports = withNx(nextTranslate(nextConfig));
--- apps/site/src/pages/_app.tsx
+++ apps/site/src/pages/_app.tsx
@@ -1,12 +1,15 @@
+import useTranslation from 'next-translate/useTranslation';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>Welcome to site!</title>
+        <title>{t('common.title')}</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
