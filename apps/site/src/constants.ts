bf40491d24d0c2bb1299382147fbbeb4b74a83a0
Siahn Pasion <71996763+siahnpasion@users.noreply.github.com>
2022-09-13 18:55:28 +0800

    Implement SimpleHash NFT api routes. (#16)

---
+++ apps/site/src/constants.ts
@@ -0,0 +1,1 @@
+export const BASE_API_URL = 'https://api.simplehash.com/api/v0';
---
+++ apps/site/src/pages/api/nfts/index.ts
@@ -0,0 +1,37 @@
+import { fetchFromApi } from './../utils';
+import { NFTListResponse } from './../../../api/types';
+import { BASE_API_URL } from '../../../constants';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NFTListResponse>
+) {
+  if (req.method !== 'GET') {
+    return res.status(400);
+  }
+
+  const { chain, chains, contract_address, query } = req.query;
+
+  let url = `${BASE_API_URL}/nfts?chains=${chains}`; // get all NFTs
+
+  if (query) {
+    url = query.toString(); // used for 'next' or 'previous' queries
+  } else if (chain && contract_address) {
+    url = `${BASE_API_URL}/nfts/${chain}/${contract_address}`; // get NFTs by Contract
+  }
+
+  await fetchFromApi(url)
+    .then((response) => response.json())
+    .then((response) => {
+      return res.status(200).json({
+        next: response.next,
+        previous: response.previous,
+        data: response.nfts,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(400);
+    });
+}
