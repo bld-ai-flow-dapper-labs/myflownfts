629ef8f9f66fc8ca72ac754f875844b58095cebd
Siahn Pasion <siahn@bld.ai>
2022-09-26 04:21:24 +0800

    Add custom title to pages.

--- apps/site/src/components/PageViewNFT/index.tsx
+++ apps/site/src/components/PageViewNFT/index.tsx
@@ -1,5 +1,6 @@
 import * as Popover from '@radix-ui/react-popover';
 import classNames from 'classnames';
+import { NextSeo } from 'next-seo';
 import useTranslation from 'next-translate/useTranslation';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -340,156 +341,164 @@ export default function PageViewNFT() {
   };
 
   return (
-    <div className="relative h-full min-h-screen bg-navbar">
-      <ToastContainer
-        position="bottom-right"
-        autoClose={1500}
-        hideProgressBar
-        pauseOnHover={false}
-        theme="dark"
-        bodyClassName="whitespace-pre-wrap"
-      />
-      <Navbar />
-      {isLoading && (
-        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-          <Loader />
-        </div>
+    <>
+      {token && (
+        <NextSeo
+          description={token.description}
+          title={t('pages.viewNFT.meta.title', { nftName: token.name })}
+        />
       )}
-
-      {!isLoading && token?.nft_id && token?.name && token?.image_url && (
-        <div className="flex flex-col h-fit w-full items-end lg:pr-[16.875rem] pt-28">
-          <div className="flex self-center justify-end w-11/12 lg:self-end lg:w-fit lg:mx-0">
-            {renderButtons()}
+      <div className="relative h-full min-h-screen bg-navbar">
+        <ToastContainer
+          position="bottom-right"
+          autoClose={1500}
+          hideProgressBar
+          pauseOnHover={false}
+          theme="dark"
+          bodyClassName="whitespace-pre-wrap"
+        />
+        <Navbar />
+        {isLoading && (
+          <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+            <Loader />
           </div>
-          <div className="flex flex-col item-center lg:flex-row w-full h-full gap-6 lg:gap-[10.375rem]">
-            <div className="flex justify-center w-11/12 pt-2 mx-auto lg:pt-0 lg:w-1/2 lg:items-start lg:justify-end lg:pb-16 h-5/6">
-              {renderNFT()}
+        )}
+
+        {!isLoading && token?.nft_id && token?.name && token?.image_url && (
+          <div className="flex flex-col h-fit w-full items-end lg:pr-[16.875rem] pt-28">
+            <div className="flex self-center justify-end w-11/12 lg:self-end lg:w-fit lg:mx-0">
+              {renderButtons()}
             </div>
-            <div className="flex justify-center w-11/12 mx-auto lg:justify-end lg:w-1/2">
-              <div className="flex flex-col min-w-[20rem] w-full gap-6 p-6 border-4 rounded-md border-container-dark/10 h-fit">
-                <div className="flex gap-3">
-                  <Image
-                    className="rounded-full"
-                    loader={() =>
-                      token.collection?.image_url ??
-                      token.previews?.image_small_url ??
-                      'Unsupported'
-                    }
-                    src={
-                      token.collection?.image_url ??
-                      token.previews?.image_small_url ??
-                      'Unsupported'
-                    }
-                    width={40}
-                    height={40}
-                    alt={token?.name}
-                    placeholder="empty"
-                    unoptimized={true}
-                  />
-                  <div
-                    className={classNames('flex flex-col w-fit max-w-[8rem]')}
-                  >
-                    <span className="block font-semibold font-body text-container-text text-body opacity-30">
-                      {t('pages.landing.createdBy')}
-                    </span>
-                    <span className="block font-semibold truncate opacity-75 font-body text-container-text text-body">
-                      {token.collection?.twitter_username ??
-                        token.collection?.name ??
-                        'Unknown'}
-                    </span>
+            <div className="flex flex-col item-center lg:flex-row w-full h-full gap-6 lg:gap-[10.375rem]">
+              <div className="flex justify-center w-11/12 pt-2 mx-auto lg:pt-0 lg:w-1/2 lg:items-start lg:justify-end lg:pb-16 h-5/6">
+                {renderNFT()}
+              </div>
+              <div className="flex justify-center w-11/12 mx-auto lg:justify-end lg:w-1/2">
+                <div className="flex flex-col min-w-[20rem] w-full gap-6 p-6 border-4 rounded-md border-container-dark/10 h-fit">
+                  <div className="flex gap-3">
+                    <Image
+                      className="rounded-full"
+                      loader={() =>
+                        token.collection?.image_url ??
+                        token.previews?.image_small_url ??
+                        'Unsupported'
+                      }
+                      src={
+                        token.collection?.image_url ??
+                        token.previews?.image_small_url ??
+                        'Unsupported'
+                      }
+                      width={40}
+                      height={40}
+                      alt={token?.name}
+                      placeholder="empty"
+                      unoptimized={true}
+                    />
+                    <div
+                      className={classNames('flex flex-col w-fit max-w-[8rem]')}
+                    >
+                      <span className="block font-semibold font-body text-container-text text-body opacity-30">
+                        {t('pages.landing.createdBy')}
+                      </span>
+                      <span className="block font-semibold truncate opacity-75 font-body text-container-text text-body">
+                        {token.collection?.twitter_username ??
+                          token.collection?.name ??
+                          'Unknown'}
+                      </span>
+                    </div>
                   </div>
-                </div>
-                <span className="font-semibold text-white text-h3">
-                  {token?.name}
-                </span>
-                <span className="font-medium text-white text-footer font-body">
-                  {token.description ?? t('pages.viewNFT.noDescription')}
-                </span>
-                {token.last_sale && (
-                  <div className="flex flex-col text-body font-semibold text-white bg-container-dark/5 rounded-md py-[1.375rem] px-4 gap-8">
-                    <div className="flex items-center justify-between pb-1.5 border-b-2 border-container-dark/5">
-                      <span>{t('pages.viewNFT.currency')}</span>
-                      <div className="flex self-end gap-1 text-white">
-                        <Button
-                          className="w-8 h-8 px-0"
-                          variant="currency"
-                          disabled={currency === 'flow'}
-                          onClick={() => setCurrency('flow')}
-                        >
-                          <FlowIcon className="scale-50" />
-                        </Button>
-                        <Button
-                          className="w-8 h-8 px-0"
-                          variant="currency"
-                          disabled={currency === 'usd'}
-                          onClick={() => {
-                            // Conservative, assumes flow token is the default currency
-                            // if (!exchangeRates && token.last_sale.unit_price) {
-                            //   setExchangeRates(await getExchangeRates());
-                            // }
-                            setCurrency('usd');
-                          }}
-                        >
-                          <USDIcon />
-                        </Button>
-                        <Button
-                          className="w-8 h-8 px-0"
-                          variant="currency"
-                          disabled={currency === 'ethereum'}
-                          onClick={() => {
-                            // if (!exchangeRates && token.last_sale.unit_price) {
-                            //   setExchangeRates(await getExchangeRates());
-                            // }
-                            setCurrency('ethereum');
-                          }}
-                        >
-                          <EthIcon className="scale-75" />
-                        </Button>
+                  <span className="font-semibold text-white text-h3">
+                    {token?.name}
+                  </span>
+                  <span className="font-medium text-white text-footer font-body">
+                    {token.description ?? t('pages.viewNFT.noDescription')}
+                  </span>
+                  {token.last_sale && (
+                    <div className="flex flex-col text-body font-semibold text-white bg-container-dark/5 rounded-md py-[1.375rem] px-4 gap-8">
+                      <div className="flex items-center justify-between pb-1.5 border-b-2 border-container-dark/5">
+                        <span>{t('pages.viewNFT.currency')}</span>
+                        <div className="flex self-end gap-1 text-white">
+                          <Button
+                            className="w-8 h-8 px-0"
+                            variant="currency"
+                            disabled={currency === 'flow'}
+                            onClick={() => setCurrency('flow')}
+                          >
+                            <FlowIcon className="scale-50" />
+                          </Button>
+                          <Button
+                            className="w-8 h-8 px-0"
+                            variant="currency"
+                            disabled={currency === 'usd'}
+                            onClick={() => {
+                              // Conservative, assumes flow token is the default currency
+                              // if (!exchangeRates && token.last_sale.unit_price) {
+                              //   setExchangeRates(await getExchangeRates());
+                              // }
+                              setCurrency('usd');
+                            }}
+                          >
+                            <USDIcon />
+                          </Button>
+                          <Button
+                            className="w-8 h-8 px-0"
+                            variant="currency"
+                            disabled={currency === 'ethereum'}
+                            onClick={() => {
+                              // if (!exchangeRates && token.last_sale.unit_price) {
+                              //   setExchangeRates(await getExchangeRates());
+                              // }
+                              setCurrency('ethereum');
+                            }}
+                          >
+                            <EthIcon className="scale-75" />
+                          </Button>
+                        </div>
+                      </div>
+                      <div className="flex -mt-[1.125rem] items-center justify-between">
+                        <span>{t('pages.viewNFT.lastSale')}</span>
+                        <Chip
+                          className="h-[1.875rem] w-fit justify-between"
+                          label={getPriceConversion().toString()}
+                          chain={currency}
+                          variant="price"
+                        />
                       </div>
                     </div>
-                    <div className="flex -mt-[1.125rem] items-center justify-between">
-                      <span>{t('pages.viewNFT.lastSale')}</span>
-                      <Chip
-                        className="h-[1.875rem] w-fit justify-between"
-                        label={getPriceConversion().toString()}
-                        chain={currency}
-                        variant="price"
-                      />
-                    </div>
-                  </div>
-                )}
-                <Button className="gap-2" href={token?.external_url}>
-                  <span>{t('pages.viewNFT.buyOnMarketplace')}</span>
-                  <LinkIcon />
-                </Button>
+                  )}
+                  <Button className="gap-2" href={token?.external_url}>
+                    <span>{t('pages.viewNFT.buyOnMarketplace')}</span>
+                    <LinkIcon />
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-      {!isLoading && !token?.nft_id && (
-        <span className="absolute font-bold text-white whitespace-pre-wrap -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-h1">
-          {t('error.pages.viewNFT.notFound')}
-        </span>
-      )}
-      {!isLoading && token?.nft_id && !token?.name && !token?.image_url && (
-        <span className="absolute font-bold text-white whitespace-pre-wrap -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-h1">
-          {t('error.pages.viewNFT.unsupported')}
-        </span>
-      )}
-
-      <Footer
-        className={classNames(
-          'pt-12',
-          (isLoading ||
-            (!isLoading &&
-              token?.nft_id &&
-              !token?.name &&
-              !token?.image_url) ||
-            (!isLoading && !token?.nft_id)) &&
-            'absolute bottom-0'
         )}
-      />
-    </div>
+        {!isLoading && !token?.nft_id && (
+          <span className="absolute font-bold text-white whitespace-pre-wrap -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-h1">
+            {t('error.pages.viewNFT.notFound')}
+          </span>
+        )}
+        {!isLoading && token?.nft_id && !token?.name && !token?.image_url && (
+          <span className="absolute font-bold text-white whitespace-pre-wrap -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-h1">
+            {t('error.pages.viewNFT.unsupported')}
+          </span>
+        )}
+
+        <Footer
+          className={classNames(
+            'pt-12',
+            (isLoading ||
+              (!isLoading &&
+                token?.nft_id &&
+                !token?.name &&
+                !token?.image_url) ||
+              (!isLoading && !token?.nft_id)) &&
+              'absolute bottom-0'
+          )}
+        />
+      </div>
+    </>
   );
 }
