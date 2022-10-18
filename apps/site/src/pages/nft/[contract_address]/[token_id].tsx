4f8a2cb8c7751e40ad41894409a16724b9f12c53
Arnel Alvaran <arnel@bld.ai>
2022-10-18 11:13:47 +0800

    Add metadata card to share link

--- apps/site/src/pages/nft/[contract_address]/[token_id].tsx
+++ apps/site/src/pages/nft/[contract_address]/[token_id].tsx
@@ -1,3 +1,26 @@
+import { BASE_API_URL, fetchFromApi } from '@myflownfts/data-access';
 import PageViewNFT from '@myflownfts/site/components/PageViewNFT';
+import type { NextApiRequest } from 'next';
 
 export default PageViewNFT;
+
+export async function getServerSideProps(req: NextApiRequest) {
+  const { contract_address, token_id } = req.query;
+  const url = `${BASE_API_URL}/nfts/flow/${contract_address}/${token_id}`;
+  let SSRToken;
+
+  await fetchFromApi(url)
+    .then((response) => response.json())
+    .then((response) => {
+      SSRToken = response;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  return {
+    props: {
+      SSRToken,
+    },
+  };
+}
